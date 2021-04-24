53b05bf9049d42a2670f6ca2c85610ed2e34c32d
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2021-04-24 13:56:22 +0300

    Test page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from data.routes import Route
@@ -7,9 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route("/index")
+def index():
+    param = {"img": "../static/img/map.png"}
+    return render_template("map_creating.html", **param)
+
+
 def main():
     db_session.global_init("db/users.db")
-    app.run()
+    app.run(port=8888, host="127.0.0.1")
 
 
 if __name__ == '__main__':
