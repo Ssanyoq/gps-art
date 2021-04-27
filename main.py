12c5a0a933f8372a9cf972d181d8a8574ab64791
Efim637 <efim.smertin@yandex.ru>
2021-04-27 16:07:27 +0300

    Registration

---
+++ main.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=3, max=15), validators.DataRequired()])
+
+    email = StringField('Email', [validators.Length(min=6, max=50), validators.Email(),
+                                  validators.DataRequired()])
+
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message="Passwords don't match")
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        username = form.username.data
+        password = generate_password_hash(str(form.password.data))
+
+        con = sqlite3.connect('users.db')
+        cur = con.cursor()
+
+        results = cur.execute("SELECT username, email FROM users").fetchall()
+
+        for res in results:
+            if res[0] == str(username) or res[1] == email:
+                if res[0] == str(username):
+                    flash('Username is already taken')
+
+                    error = 'Username is already taken'
+                    return render_template('register.html', error=error, form=form)
+
+                elif res[1] == email:
+                    flash('This email is already registered')
+
+                    error = 'This email is already registered'
+                    return render_template('register.html', error=error, form=form)
+
+        cur.execute("INSERT INTO users(email, username, hashed_password) VALUES(?, ?, ?);",
+                    (email, username, password))
+
+        con.commit()
+        con.close()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username', False)
+        password_check = request.form.get('password', False)
+        password = ''
+
+        con = sqlite3.connect('users.db')
+        cur = con.cursor()
+
+        result = cur.execute(f"""SELECT username, hashed_password FROM users""").fetchall()
+
+        for res in result:
+            if res[0] == username:
+                password = res[1]
+
+        if password == '':
+            flash('Invalid username')
+
+        elif check_password_hash(password, password_check):
+            session['logged_in'] = True
+            session['username'] = username
+
+            flash('You are now logged in', 'success')
+
+            # return redirect(url_for('register'))
+        else:
+            flash('Invalid password')
+
+        cur.close()
+
+    return render_template('login.html')
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login')
+            return redirect(url_for('login'))
+
+    return wrap
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out')
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.secret_key = 'secret123'
+    app.run(debug=True)
